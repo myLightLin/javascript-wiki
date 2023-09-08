787deb92621b33f1f616ae8a0a8a6a01b3853cc4
lightlin <lighterlam@gmail.com>
2023-09-08 21:26:17 +0800

    feat: add addThousandSeparator function

---
+++ src/addThousandSeparator.js
@@ -0,0 +1,26 @@
+
+/**
+ * 给数字添加千分位分隔符
+ * @param {number} num
+ * @returns {string}
+ */
+export default function addThousandSeparator(num) {
+  const numStr = num.toString().split('.')
+  const [integerPart, decimalPart] = numStr
+  let result = ''
+
+  let count = 0
+  for (let i = integerPart.length; i >= 0; i--) {
+    count++
+    result = integerPart[i] + result
+    if (count % 3 === 0 && i !== 0) {
+      result = ',' + result
+    }
+  }
+
+  if (decimalPart) {
+    result = result + '.' + decimalPart
+  }
+
+  return result
+}
