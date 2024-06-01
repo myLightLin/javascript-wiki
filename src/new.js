4ed93b3730516cdae3b9d423eda620deb2cefbf3
lightlin <lighterlam@gmail.com>
2024-06-01 17:59:40 +0800

    chore: modify new function

--- src/new.js
+++ src/new.js
@@ -1,10 +1,16 @@
 /* eslint-disable prefer-rest-params */
-// 模拟实现 new
+/**
+ * new 的实现
+ * 1. 创建一个空对象
+ * 2. 将空对象的原型指向构造函数的原型
+ * 3. 将构造函数的 this 指向这个空对象并执行构造函数
+ * 4. 如果 3 的结果是对象，则返回这个对象，否则返回 1 中创建的对象
+ */
 export default function mockNew() {
-  const obj = {} // 创建空对象
-  const constructor = [].shift.call(arguments) // 获取传入的构造函数
+  const obj = {}
+  const constructor = Array.prototype.shift.call(arguments)
   // eslint-disable-next-line no-proto
-  obj.__proto__ = constructor.prototype // 设置原型
-  const ret = constructor.apply(obj, arguments) // 执行构造函数
-  return typeof ret === 'object' ? ret : obj // 判断返回结果
+  obj.__proto__ = constructor.prototype
+  const ret = constructor.apply(obj, arguments)
+  return typeof ret === 'object' ? ret : obj
 }
